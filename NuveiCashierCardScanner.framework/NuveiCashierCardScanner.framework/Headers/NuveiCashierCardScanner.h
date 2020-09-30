4be880ce03df306bb19dd0e9ccf0cb5d4e046a27
Michael Kessler <michael@mobaxe.com>
2020-09-30 12:09:18 +0300

    v0.0.2

---
+++ NuveiCashierCardScanner.framework/NuveiCashierCardScanner.framework/Headers/NuveiCashierCardScanner.h
@@ -0,0 +1,31 @@
+//
+//  NuveiCashierCardScanner.h
+//  NuveiCashierCardScannerSDK
+//
+//  Created by Michael Kessler on 09/09/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+@class WKWebView;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NuveiCashierCardScanner : NSObject
+
++ (void)connectTo:(WKWebView *)webView;
+
+@end
+
+//@interface SCCardIOCreditCardInfo : NSObject
+//
+//@end
+//
+//@interface SCCardIOUtilities : NSObject
+//
+//+ (BOOL)canReadCardWithCamera;
+//+ (void)preloadCardIO;
+//
+//@end
+
+NS_ASSUME_NONNULL_END
